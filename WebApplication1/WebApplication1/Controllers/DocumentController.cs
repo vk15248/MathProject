811be957a7c82d15903d063471e728e495624b55
vk15248 <vedranakadic@gmail.com>
2016-01-24 12:31:33 +0100

    added api for documents

---
+++ WebApplication1/WebApplication1/Controllers/DocumentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DocumentController : ApiController
+    {
+        Document[] documents = new Document[]
+        {
+            new Document { Id = 1, Name = "Tomato Soup", Category = "Groceries", Number = 1, ChangeDate = DateTime.Today },
+            new Document { Id = 2, Name = "Yo-yo", Category = "Toys", Number = 1, ChangeDate = DateTime.Today},
+            new Document { Id = 3, Name = "Hammer", Category = "Hardware",Number = 1, ChangeDate = DateTime.Today }
+        };
+
+        // GET: api/Document
+        public IEnumerable<Document> Get()
+        {
+            return documents;
+        }
+
+        // GET: api/Document/5
+        public Document Get(int id)
+        {
+            return null;
+        }
+
+        // POST: api/Document
+        public void Post([FromBody]Document value)
+        {
+        }
+
+        // PUT: api/Document/5
+        public void Put(int id, [FromBody]Document value)
+        {
+        }
+
+        // DELETE: api/Document/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Dokument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public String FileName { get; set; }
+        public int Number { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public String Description { get; set; }
+        public int UserId { get; set; }
+        public String Class { get; set; }
+        public String Category { get; set; }
+        public String Name { get; set; }
+    }
+}
