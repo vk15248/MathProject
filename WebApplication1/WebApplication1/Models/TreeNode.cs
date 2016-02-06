fb7a14416c5acc072289292a7ff89455babce54b
vk15248 <vedranakadic@gmail.com>
2016-02-06 11:58:01 +0100

    populated the tree with actual data, made some css changes

---
+++ WebApplication1/WebApplication1/Models/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class TreeNode
+    {
+        public Int32 id { get; set; }
+        public String text { get; set; }
+        public String icon { get; set; }
+    }
+}
